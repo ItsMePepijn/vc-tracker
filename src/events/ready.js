00af742cb915436a927e02ac184d50052ea2d44a
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-11-22 09:10:32 +0100

    newline

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,5 +4,6 @@ module.exports = {
   execute(client){
       console.log(`${client.user.tag} Has logged in!`);
       console.log('Invite Link: ' + `https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`);
+      console.log(' ');
   },
 };
