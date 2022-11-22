bb5b5e8cf9efe771a10f387c0ebf1cec43235cbb
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-11-22 09:39:59 +0100

    added commands to invite

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,7 +3,7 @@ module.exports = {
   once: true,
   execute(client){
       console.log(`${client.user.tag} Has logged in!`);
-      console.log('Invite Link: ' + `https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`);
+      console.log('Invite Link: ' + `https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot%20applications.commands&permissions=8`);
       console.log(' ');
   },
 };
