adbdea290b0606061396db8d5056338018292376
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-11-24 11:51:12 +0100

    refactored to npm script

--- deploy-commands/deploy.js
+++ deploy-commands/deploy.js
@@ -17,15 +17,27 @@ for (const folder of commandFolders) {
 }
 
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+const method = process.argv[2];
+if(method !== 'dev' && method !== 'prod') return console.error(chalk.red('ERR: Please specify a method: dev or prod'));
 
 (async () => {
 	try {
 		console.log(chalk.yellow(`Started refreshing ${commands.length} application (/) commands.`));
+		console.log(chalk.yellow(`METHOD: ${method}`));
 
-		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, devServerId),
-			{ body: commands },
-		);
+		let data;
+		if(method === 'dev') {
+			data = await rest.put(
+				Routes.applicationGuildCommands(clientId, devServerId),
+				{ body: commands },
+			);
+		} 
+		else {
+			data = await rest.put(
+				Routes.applicationCommands(clientId),
+				{ body: commands },
+			);
+		}
 
 		console.log(chalk.green(`Successfully reloaded ${data.length} application (/) commands.`));
 	} catch (error) {
