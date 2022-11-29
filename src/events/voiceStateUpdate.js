034eb6730b7a90713fc760c99fbbe8302253c138
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-11-29 09:20:40 +0100

    check for global tracking

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,9 +1,17 @@
 const voiceModules = require('../modules/voice.js');
+const { QuickDB } = require('quick.db');
+const userPreferences = new QuickDB({ filePath: './db/userPreferences.sqlite', table: 'userPreferences' });
+
 
 module.exports = {
   name: 'voiceStateUpdate',
-  execute(oldState, newState, client) {
+  async execute(oldState, newState) {
+    const userPreference = await userPreferences.get(newState.id);
+    if(!userPreference || (!userPreference.globalTracking.isEnabled && !userPreference.globalTracking.asked)){
+      voiceModules.getPermission(newState);
+      await userPreferences.set(`${newState.id}.globalTracking.asked`, true);
+    }
+
     voiceModules.userUpdate(oldState, newState);
-    
   }
 }
--- src/modules/voice.js
+++ src/modules/voice.js
@@ -1,6 +1,8 @@
 const { QuickDB } = require('quick.db');
 const vcStats = new QuickDB({ filePath: './db/userData.sqlite', table: 'vcStats' });
 const serverData = new QuickDB({ filePath: './db/userData.sqlite', table: 'serverData' });
+const userPreferences = new QuickDB({ filePath: './db/userPreferences.sqlite', table: 'userPreferences' });
+const Discord = require('discord.js');
 
 async function userUpdate(oldState, newState) {
   if(!await serverData.get(newState.guild.id)) await serverData.set(newState.guild.id, { shouldBeUnmuted: true, shouldBeUndeafened: true });
@@ -35,6 +37,29 @@ async function userUpdate(oldState, newState) {
   }
 }
 
+async function getPermission(newState) {
+  const embed = new Discord.EmbedBuilder()
+    .setTitle('Voice Channel Tracking')
+    .setDescription('Do you want your vc time to be tracked for the global leaderboard?\n*This will only track your time in servers I am in*\n\n**You can change this at any time with `/globaltracking`**')
+    .setColor('#FF0000')
+    .setTimestamp();
+
+  const actionRow = new Discord.ActionRowBuilder()
+    .addComponents([
+      new Discord.ButtonBuilder()
+        .setLabel("Yes!")
+        .setCustomId("enableGlobalTracking")
+        .setStyle(3),
+      new Discord.ButtonBuilder()
+        .setLabel("No")
+        .setCustomId("disableGlobalTracking")
+        .setStyle(4)
+    ])
+  
+  const user = await newState.guild.members.fetch(newState.id)
+  const message = user.send({ embeds: [embed], components: [actionRow] });
+}
 module.exports = {
-  userUpdate
+  userUpdate,
+  getPermission
 }
