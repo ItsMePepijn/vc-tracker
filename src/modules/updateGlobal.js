fe17c34b42de156e02c9520da9c21001274449c5
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-12-02 13:08:18 +0100

    now checking for preferences

--- src/modules/updateGlobal.js
+++ src/modules/updateGlobal.js
@@ -1,19 +1,17 @@
 const { QuickDB } = require('quick.db');
 const vcStats = new QuickDB({ filePath: './db/userData.sqlite', table: 'vcStats' });
 const leaderboardDatabase = new QuickDB({ filePath: './db/userData.sqlite', table: 'globalLeaderboard' });
+const userPreferences = new QuickDB({ filePath: './db/userPreferences.sqlite', table: 'userPreferences' });
 
 module.exports = async () => {
   const all = await vcStats.all()
   let leaderboard = []
 
   await all.forEach(server => {
-    Object.entries(server.value).forEach(user => {
-      console.log(user)
-      leaderboard.push({id: user[0], time: user[1].time})
+    Object.entries(server.value).forEach(async user => {
+      if(await userPreferences.get(`${user[0]}.globalTracking.isEnabled`) === true) leaderboard.push({id: user[0], time: user[1].time})
     })
   })
-
-  console.log(leaderboard)
 
   await leaderboard.sort((a, b) => b.time - a.time);
   if(leaderboard.length > 99) leaderboard = leaderboard.slice(0, 99);
