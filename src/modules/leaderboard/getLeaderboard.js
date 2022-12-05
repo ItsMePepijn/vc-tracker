349bef521b90d059b47d2f2d89cc3d9006791f79
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-12-05 13:03:35 +0100

    better errors and pages

--- src/modules/leaderboard/getLeaderboard.js
+++ src/modules/leaderboard/getLeaderboard.js
@@ -3,18 +3,20 @@ const vcStats = new QuickDB({ filePath: './db/userData.sqlite', table: 'vcStats'
 const globalLeaderboard = new QuickDB({ filePath: './db/userData.sqlite', table: 'globalLeaderboard' });
 
 
-module.exports = async (serverId, tab = 0) => {
+module.exports = async ({serverId, tab = 0} = {serverId: undefined, tab: 0}) => {
+  let leaderboardData
   if(serverId){
     const serverData = await vcStats.get(serverId);
-    if (!serverData) return null;
+    if (!serverData) return {error: 'There is no leaderboard for this server.'};
 
-    const leaderboardData = await Object.entries(serverData).sort((a, b) => b[1].time - a[1].time);
-    return leaderboardData;
+    leaderboardData = await Object.entries(serverData).sort((a, b) => b[1].time - a[1].time);
   }
   else{
-    const leaderboardData = await globalLeaderboard.get('leaderboard');
-    if (!leaderboardData) return null;
+    leaderboardData = await globalLeaderboard.get('leaderboard');
 
     if (!leaderboardData[0]) return {error: 'There is no global leaderboard yet, you can be the first on it!'};
   }
+  if(!leaderboardData[tab * 10]) return {error: 'This page does not exist!'};
+
+  return leaderboardData.slice(tab * 10, (tab * 10) + 10);
 }
