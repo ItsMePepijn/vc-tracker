b2ea77f1e58a456bccb1ba4c11881ccce637345f
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-12-06 13:29:53 +0100

    added debugging to startup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const startup = require('./modules/setup/startup.js');
 const {client} = require('./modules/setup/client');
 require('dotenv').config();
 
+//Debug
+const debug = process.argv[2] === 'debug' ? true : false;
+if(debug){
+  console.log('Running in debug mode');
+  client.on('debug', console.log)
+  client.on('warn', console.warn);
+}
+
 //Commands setup
 setup.commands.load(client);
 
@@ -12,6 +20,5 @@ setup.events.load(client);
 
 //Startup
 startup.run();
-
 
 client.login(process.env.BOT_TOKEN);
