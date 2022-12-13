9cbcabf1b5b9857d92a0bbf797aed02f34dc23ea
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-12-13 09:12:50 +0100

    moved voice modules to seperate files

---
+++ src/modules/voice/getPermission.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
+
+module.exports = async function getPermission(newState) {
+  const embed = new EmbedBuilder()
+    .setTitle('Voice Channel Tracking')
+    .setDescription('Do you want your vc time to be tracked for the global leaderboard?\n*This will only track your time in servers I am in*\n\n**You can change this at any time with `/globaltracking`**')
+    .setColor('#2f3136')
+    .setTimestamp();
+
+  const actionRow = new ActionRowBuilder()
+    .addComponents([
+      new ButtonBuilder()
+        .setLabel("Yes!")
+        .setCustomId("enableGlobalTracking")
+        .setStyle(3),
+      new ButtonBuilder()
+        .setLabel("No")
+        .setCustomId("disableGlobalTracking")
+        .setStyle(4)
+    ])
+  
+  const user = await newState.guild.members.fetch(newState.id)
+  user.send({ embeds: [embed], components: [actionRow] }).catch(() => {});
+}
---
+++ src/modules/voice/permissionButtonClick.js
@@ -0,0 +1,23 @@
+const { QuickDB } = require('quick.db');
+const userPreferences = new QuickDB({ filePath: './db/userPreferences.sqlite', table: 'userPreferences' });
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = async function permissionButtonClick(interaction) {
+  const embed = new EmbedBuilder()
+    .setTitle('Voice Channel Tracking')
+    .setTimestamp();
+
+  if(interaction.customId === 'enableGlobalTracking') {
+    await userPreferences.set(`${interaction.user.id}.globalTracking.isEnabled`, true);
+    embed.setDescription('Global leaderboard tracking has been enabled!\n*This will only track your time in servers I am in*')
+      .setColor('#43b581');
+    return interaction.message.edit({ embeds: [embed], components: [] });
+  }
+
+  if(interaction.customId === 'disableGlobalTracking') {
+    await userPreferences.set(`${interaction.user.id}.globalTracking.isEnabled`, false);
+    embed.setDescription('Global leaderboard tracking will not be turned on\nYou can turn this on at any time with `/globaltracking`')
+      .setColor('#f04747');
+    return interaction.message.edit({ embeds: [embed], components: [] });
+  }
+}
