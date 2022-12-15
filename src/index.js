c2c729569250529872305389ab4c9b10fde4c9b5
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-12-15 11:33:55 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ const setup = require('./modules/setup');
 require('dotenv').config();
 
 //Debug
-const debug = process.argv[2] === 'debug' ? true : false;
-if(debug){
+if(process.argv[2] === 'debug'){
   console.log('Running in debug mode');
   setup.client.on('debug', console.log)
   setup.client.on('warn', console.warn);
