5d65e6019002dedd37120b3bc7a224ea936d133c
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-12-19 13:09:52 +0100

    updates a userpreferenece

---
+++ src/modules/database/setPreference.js
@@ -0,0 +1,17 @@
+const { userPreference } = require('./models')
+
+module.exports = async (id, newPreference) => {
+
+  if (!(await userPreference.findOne({ _id: id }))) {
+    preference = new userPreference({
+      _id: id,
+      isAsked: true,
+      isEnabled: newPreference,
+    })
+    await preference.save()
+  }
+  else{
+    await userPreference.updateOne({ _id: id }, {"globalTracking.isEnabled": newPreference})
+  }
+
+}
